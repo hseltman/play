1bb13dff9904b04b1b4ace90c633dac2974bdf82
Howard Seltman <hseltman@stat.cmu.edu>
2018-06-26 08:31:13 -0400

    enhance 1

--- random.R
+++ random.R
@@ -1,8 +1,12 @@
 # print random numbers
 
 N = 10
+cat("Simulating", N, "normals\n")
 x=rnorm(N)
 print(x)
+
+# Enhancements
+cat("mean(x) =", formatC(mean(x), digits=2, format="f"), "\n")
 
 # New features
 y = sample(10:20, N)
